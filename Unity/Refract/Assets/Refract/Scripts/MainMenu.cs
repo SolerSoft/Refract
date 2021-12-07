c7488f0b99a118e14f75ef20ec0e4f60715a3332
jbienz <jbienz@hotmail.com>
2021-12-06 20:19:01 -0600

    FPS and Version working.

--- Unity/Refract/Assets/Refract/Scripts/MainMenu.cs
+++ Unity/Refract/Assets/Refract/Scripts/MainMenu.cs
@@ -13,6 +13,15 @@ namespace Refract
     /// </summary>
     public class MainMenu : MonoBehaviour
     {
+        #region Constants
+        private float FPS_UPDATE_RATE = 0.5f;
+        #endregion // Constants
+
+        #region Member Variables
+        private bool isShown;
+        private float lastFPSUpdate;
+        #endregion // Member Variables
+
         #region Unity Inspector Variables
         [Header("Controllers")]
         [SerializeField]
@@ -28,10 +37,18 @@ namespace Refract
         private MenuController menuController;
 
 
-        [Header("Control")]
+        [Header("Controls")]
         [SerializeField]
         [Tooltip("The slider that adjusts the depthiness of the projector.")]
         private PinchSlider depthinessSlider;
+
+        [SerializeField]
+        [Tooltip("The control that displays the frame rate.")]
+        private TextMesh fpsText;
+
+        [SerializeField]
+        [Tooltip("The control that displays the version.")]
+        private TextMesh versionText;
 
 
         [Header("Settings")]
@@ -41,11 +58,42 @@ namespace Refract
         #endregion // Unity Inspector Variables
 
         #region Internal Methods
+        /// <summary>
+        /// Converts a percentage to a range value.
+        /// </summary>
+        /// <param name="min">
+        /// The minimum value in a range.
+        /// </param>
+        /// <param name="max">
+        /// The maximum value in a range.
+        /// </param>
+        /// <param name="percent">
+        /// The percentage to convert.
+        /// </param>
+        /// <returns>
+        /// The ranged value.
+        /// </returns>
         static private float PercentToRange(float min, float max, float percent)
         {
             float r = (max - min);
             return (r * percent) + min;
         }
+
+        /// <summary>
+        /// Converts a ranged value to a percentage
+        /// </summary>
+        /// <param name="min">
+        /// The minimum value in a range.
+        /// </param>
+        /// <param name="max">
+        /// The maximum value in a range.
+        /// </param>
+        /// <param name="range">
+        /// The ranged value to convert.
+        /// </param>
+        /// <returns>
+        /// The percentage.
+        /// </returns>
         static private float RangeToPercent(float min, float max, float range)
         {
             float r = (max - min);
@@ -58,6 +106,7 @@ namespace Refract
         /// </summary>
         private void SceneToControls()
         {
+            // Sliders
             depthinessSlider.SliderValue = RangeToPercent(HoloController.DEPTHINESS_MIN, HoloController.DEPTHINESS_MAX, holoController.Depthiness);
         }
         #endregion // Internal Methods
@@ -68,6 +117,9 @@ namespace Refract
         /// </summary>
         private void MainMenu_Hidden()
         {
+            // No longer shown
+            isShown = false;
+
             // Unsubscribe from control events
             depthinessSlider.OnValueUpdated.RemoveListener(Depthiness_SliderChanged);
 
@@ -91,6 +143,9 @@ namespace Refract
             {
                 holoController.Projector.gameObject.SetActive(false);
             }
+
+            // Shown
+            isShown = true;
         }
 
         /// <summary>
@@ -109,7 +164,9 @@ namespace Refract
         /// <inheritdoc/>
         protected virtual void Start()
         {
+            menuController.Title = Application.productName;
 
+            if (versionText != null) { versionText.text = $"v{Application.version}"; }
         }
 
         /// <inheritdoc/>
@@ -124,6 +181,17 @@ namespace Refract
         {
             menuController.Hidden.AddListener(MainMenu_Hidden);
             menuController.Shown.AddListener(MainMenu_Shown);
+        }
+
+        /// <inheritdoc/>
+        protected virtual void Update()
+        {
+            if ((isShown) && (fpsText != null) && ((Time.unscaledTime - lastFPSUpdate) > FPS_UPDATE_RATE))
+            {
+                lastFPSUpdate = Time.unscaledTime;
+                int fps = (int)(1f / Time.unscaledDeltaTime);
+                fpsText.text = $"{fps} FPS";
+            }
         }
         #endregion // Unity Overrides
 
